570db32d4ca40f543f7f19f8f7cc912d8dbb60a1
nAgI314 <kurosawasyunnagi@gmail.com>
2024-12-13 16:03:41 +0900

    first2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/SnapSum/',
+    base: '/NewSnapSum/',
     plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
     build: {
         rollupOptions: {
